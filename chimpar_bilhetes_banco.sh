0b451b0b0484176ab6eabd123ab71dfd133f31b8
O Fantástico Senhor Chimposo <65960679+devchimpa@users.noreply.github.com>
2023-01-10 14:45:25 -0300

    Adicionado interatividade do macaco
    Agora temos um macaquinho para ajudar a achar bilhetes.

--- chimpar_bilhetes_banco.sh
+++ chimpar_bilhetes_banco.sh
@@ -10,25 +10,56 @@
 # 3 - descompactar
 # 4 - dar um grep para saber se tem bilhete.
 # 5 - vai pegar os bilhetes e transformar em txt
-# 6 - pegar os bilhetes do txt e cortar na parte do insert e gerar o arquivo final.
+# 6 - pegar os bilhetes do txt e cortar nai parte do insert
 # 7 - fim do script
 #
 #Conta do operador para teste 7070
 #
 #Desenvolvido por: DevChimpa 10-01-2023
 #contato: chimpadeveloper@gmail.com
-###########################################################################
+#
+##########################################################################
 
 CAMINHO_ORIGEM="/home/backups/"
-ARQUIVO_ZIP=$1
-FILTRO_GREP=$2
 
-#clear
-#echo "########################################"
-#echo "insira as datas dos bilhetes desejados"
-#echo "########################################"
+clear
+echo "##################################################"
+echo "           --------------------------------------"
+echo "          insira as datas dos bilhetes desejados:"
+echo "         /--------------------------------------"
+echo "        /
+     /~\
+    C oo)   -----
+    _( ^)  /    /
+   /__m~\m/____/ "
+echo "#################################################"
+read ARQUIVO_ZIP
 
+clear
+echo "######################################################"
+echo "           ------------------------------------------"
+echo "          insira a informação que gostaria de filtrar:"
+echo "         /-------------------------------------------"
+echo "        /
+     /~\
+    C oo)   -----
+    _( ^)  /    /
+   /__m~\m/____/ "
+echo "#################################################"
+read FILTRO_GREP
 
+clear
+echo "######################################################"
+echo "           --------------------------"
+echo "          Só um momento por favor..."
+echo "         /---------------------------"
+echo "        /
+     /~\
+    C oo)   -----
+    _( ^)  /    /
+   /__m~\m/____/ "
+echo "#################################################"
+sleep 5
 # inicialmente o script está pegando como primeiro parametro os bilhetes
 # e como segundo parametro o filtro que deve ser aplicado.
 # futuramente pretendo solicitar cada parametro separadamente
@@ -44,14 +75,28 @@ cd $CAMINHO_ORIGEM
 cp $ARQUIVO_ZIP $CAMINHO_DESTINO
 
 cd $CAMINHO_DESTINO
-tar -zxvf $ARQUIVO_ZIP
-tar -xvf *.tar
+tar -zxf $ARQUIVO_ZIP
+tar -xf *.tar
 
 grep -ir $FILTRO_GREP >> bilhetes_salvos.txt
 
 cat bilhetes_salvos.txt | awk -F "INSERT INTO" '{print "INSERT INTO"$2}' > inserts.txt
 
 rm bilhetes_salvos.txt
+
+clear
+echo "######################################################"
+echo "           -------------------------------------------"
+echo "          Prontinho. Arquivo inserts em /tmp/bilhetes/"
+echo "         /--------------------------------------------"
+echo "        /
+     /~\
+    C oo)   -----
+    _( ^)  /    /
+   /__m~\m/____/ "
+echo "#################################################"
+sleep 5
+clear
 
 #ls $CAMIMNHO_DESTINO
 #$CAMINHO_ORIGEM
