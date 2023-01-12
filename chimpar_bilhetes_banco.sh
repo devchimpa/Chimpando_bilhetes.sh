261badc39f98d8aa4d7e3935357c112e5d2b00c3
O Fantástico Senhor Chimposo <65960679+devchimpa@users.noreply.github.com>
2023-01-12 17:20:20 -0300

    Atualizações e melhorias no código.

--- chimpar_bilhetes_banco.sh
+++ chimpar_bilhetes_banco.sh
@@ -3,20 +3,15 @@
 # Script feito com o objetivo de acelerar o processo
 #de inserir bilhetes de chamadas no banco.
 #
-#informacoes importantes sobre o que o script deve fazer...
 #
-# 1 - ele vai pegar o arquivo da pasta de backup
-# 2 - mandar para a tmp
-# 3 - descompactar
-# 4 - dar um grep para saber se tem bilhete.
-# 5 - vai pegar os bilhetes e transformar em txt
-# 6 - pegar os bilhetes do txt e cortar nai parte do insert
-# 7 - fim do script
-#
-#Conta do operador para teste 7070
 #
 #Desenvolvido por: DevChimpa 10-01-2023
 #contato: chimpadeveloper@gmail.com
+#
+# Atualização 12-01-2023
+# Atualizado a forma como se busca bilhetes para tentar reduzir os erros.
+# próximos passos, testar se foi inserido valor nas variáveis ARQUIVO_ENTRADA e FILTRO_GREP
+# caso FILTRO_GREP esteja vazio, substituir por insert
 #
 ##########################################################################
 
@@ -25,7 +20,7 @@ CAMINHO_ORIGEM="/home/backups/"
 clear
 echo "##################################################"
 echo "           --------------------------------------"
-echo "          insira a data do bilhete desejado "
+echo "          Insira os arquivos de bilhetes.       "
 echo "         /--------------------------------------"
 echo "        /
      /~\
@@ -33,66 +28,106 @@ echo "        /
     _( ^)  /    /
    /__m~\m/____/ "
 echo "#################################################"
-echo "Exemplo: 2023-01-01"
+echo "Exemplos: 2023-01-01.tar.gz  ou  2023-01-*       "
 echo "#################################################"
 ls -m $CAMINHO_ORIGEM
-read ARQUIVO_ZIP
-#echo "$ARQUIVO_ZIP 1"
+read ARQUIVO_ENTRADA
 clear
-echo "######################################################"
-echo "           ------------------------------------------"
-echo "          insira a informação que gostaria de filtrar:"
-echo "         /-------------------------------------------"
+
+clear
+echo "##################################################"
+echo "           --------------------------------------"
+echo "          Qual informação gostaria de filtrar?   "
+echo "         /--------------------------------------"
 echo "        /
      /~\
     C oo)   -----
     _( ^)  /    /
    /__m~\m/____/ "
-echo "#################################################"
-echo " Exemplos: Conta, ramal, ou tipo de bilhete "
 echo "#################################################"
 read FILTRO_GREP
-
 clear
-echo "######################################################"
-echo "           --------------------------"
-echo "          Só um momento por favor..."
-echo "         /---------------------------"
+
+echo "##################################################"
+echo "           --------------------------------------"
+echo "          Um momento por favor...                "
+echo "         /--------------------------------------"
 echo "        /
      /~\
     C oo)   -----
     _( ^)  /    /
    /__m~\m/____/ "
 echo "#################################################"
-sleep 3
-clear
-#
-if [ ! -d /tmp/bilhetes ]
-then    mkdir -p /tmp/bilhetes
-CAMINHO_DESTINO="/tmp/bilhetes"
-else
-        CAMINHO_DESTINO="/tmp/bilhetes"
-fi
 
-cd $CAMINHO_ORIGEM
-cp -v $ARQUIVO_ZIP* $CAMINHO_DESTINO
-#echo "$ARQUIVO_ZIP 2"
-cd $CAMINHO_DESTINO
-tar -zxvf "$ARQUIVO_ZIP.tar.gz"
-#echo "$ARQUIVO_ZIP 3"
-sleep 5
-tar -xvf *.tar
+echo $ARQUIVO_ENTRADA >> /tmp/listadebilhetes.txt
+cat -T /tmp/listadebilhetes.txt | tr ' ' '\n' > /tmp/nova_lista.txt
+sleep 2
+rm /tmp/listadebilhetes.txt
 
+#cat /tmp/nova_lista.txt
 
-grep -ir $FILTRO_GREP >> bilhetes_salvos.txt
+ARQUIVO_PRIMARIO="/tmp/nova_lista.txt"
+while read PRIMEIRAENTRADA;
+do
+        cd $CAMINHO_ORIGEM
+        ls "$CAMINHO_ORIGEM" | grep "$PRIMEIRAENTRADA" > /tmp/lista_pasta.txt
+       # ls "$CAMINHO_ORIGEM$PRIMEIRAENTRADA" > /tmp/lista_pasta.txt
+        sort /tmp/lista_pasta.txt | uniq | grep / -v > /tmp/sembranco.txt
+        awk 'NF>0' /tmp/sembranco.txt > /tmp/ListaFinal.txt
 
-cat bilhetes_salvos.txt | awk -F "INSERT INTO" '{print "INSERT INTO"$2}' > inserts.txt
+done < $ARQUIVO_PRIMARIO
 
-rm bilhetes_salvos.txt
+rm /tmp/lista_pasta.txt
+rm /tmp/nova_lista.txt
+
+ARQUIVO_TEXTO="/tmp/ListaFinal.txt"
+
+while read ARQUIVOLINHA;
+do
+        #echo "Arquivo: $ARQUIVOLINHA"
+        cd $CAMINHO_ORIGEM
+                if [ ! -d /tmp/bilhetes ]
+                then    mkdir -p /tmp/bilhetes
+                CAMINHO_DESTINO="/tmp/bilhetes"
+                else
+                CAMINHO_DESTINO="/tmp/bilhetes"
+                fi
+        echo "Copiando ... "
+        sleep 2
+        cp -v $ARQUIVOLINHA /tmp/bilhetes
+        cd $CAMINHO_DESTINO
+        echo "Desarquivando ..."
+        sleep 2
+        tar -zxvf $ARQUIVOLINHA
+        sleep 2
+        rm $ARQUIVOLINHA
+        ARQUIVOTAR=$(ls *.tar)
+        echo "Descompactando..."
+        sleep 2
+        tar -xvf $ARQUIVOTAR
+        sleep 2
+        echo "Procurando bilhetes..."
+        rm $ARQUIVOTAR
+        sleep 2
+        grep -ir $FILTRO_GREP >> /tmp/grepbilhetes.txt
+        PASTA=$(ls )
+        rm -r $PASTA
+        cd /tmp
+        cat grepbilhetes.txt | awk -F "INSERT INTO" '{print "INSERT INTO"$2}' > inserts.txt
+        rm grepbilhetes.txt
+
+done < $ARQUIVO_TEXTO
+
+sleep 2
+
+rm -r /tmp/bilhetes
+rm -r /tmp/sembranco.txt
+rm -r /tmp/ListaFinal.txt
+
 
 echo "######################################################"
 echo "           -------------------------------------------"
-echo "          Pronto. Arquivo inserts.txt em /tmp/bilhetes/"
+echo "          Pronto. Arquivo inserts.txt em /tmp"
 echo "         /--------------------------------------------"
 echo "        /
      /~\
@@ -100,11 +135,3 @@ echo "        /
     _( ^)  /    /
    /__m~\m/____/ "
 echo "#################################################"
-
-
-
-#ls $CAMIMNHO_DESTINO
-#$CAMINHO_ORIGEM
-
-#cd $CAMINHO_ORIGEM
-#echo $ARQUIVO_ZIP
